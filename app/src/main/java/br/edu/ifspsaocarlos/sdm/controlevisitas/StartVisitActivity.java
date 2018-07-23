a0e6ded6fffcb575813555498dc41d7210667168
flpfar <flp.far@gmail.com>
2018-07-22 23:50:43 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/controlevisitas/StartVisitActivity.java
@@ -0,0 +1,13 @@
+package br.edu.ifspsaocarlos.sdm.controlevisitas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartVisitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_visit);
+    }
+}
